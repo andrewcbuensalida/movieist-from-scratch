7255c53e69b3c1b827f7c7b955b2c555d82c2ad0
Andrew Buensalida <andrewcbuensalida@gmail.com>
2024-01-18 20:42:41 -0800

    reviews

--- src/main/java/com/movieistfromscratch/movieist/MovieistApplication.java
+++ src/main/java/com/movieistfromscratch/movieist/MovieistApplication.java
@@ -2,12 +2,29 @@ package com.movieistfromscratch.movieist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication // This single annotation is equivalent to using @Configuration, @EnableAutoConfiguration, and @ComponentScan. As a result, when we run this Spring Boot application, it will automatically scan the components in the current package and its sub-packages. Thus it will register them in Spring’s Application Context, and allow us to inject beans using @Autowired. we can use autowiring on properties, setters, and constructors. When we annotate a property using @Autowired, this searches the spring container for any objects (aka beans) aka classes that are annotated with @Component. If it was annotated with @Component("laptop1"), can use @Autowired @Qualifier("laptop1") to specify which object, in case there are more than one.
+@RestController
 public class MovieistApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MovieistApplication.class, args);
+  } // run creates a spring container which will create an object (aka bean) of the classes that you annotate with @Component. if you annotate the class with @Scope(value="prototype"), it will only create an object if you context.getBean it.
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false).maxAge(3600);
+      }
+    };
   }
-
 }
---
+++ src/main/java/com/movieistfromscratch/movieist/movies/Movie.java
@@ -0,0 +1,39 @@
+package com.movieistfromscratch.movieist.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies") // let's mongodb know this is a collection
+@Data // takes care of getters/setters/toString methods
+@AllArgsConstructor // requires all the properties to be initialized when doing new
+@NoArgsConstructor // if allows object to be initialized without arguments, it puts default values
+public class Movie {
+    @Id // lets mongodb know this is the unique id for each movie
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> backdrops;
+    private List<String> genres;
+    @DocumentReference // only saves the foreign key, not the actual review. reviews will be in a separate collection.
+    private List<Review> reviews;
+
+    public Movie(String imdbId, String title, String releaseDate, String trailerLink, String poster, List<String> backdrops, List<String> genres) {
+        this.imdbId = imdbId;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.trailerLink = trailerLink;
+        this.poster = poster;
+        this.backdrops = backdrops;
+        this.genres = genres;
+    }
+}
